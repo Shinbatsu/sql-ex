ac6d181f4f0c92eb93a75e2b7af8dce9cb883988
shinbatsu <nojic132@gmail.com>
2019-07-07 19:44:22 +0300

    solve 98

---
+++ 98.sql
@@ -0,0 +1,15 @@
+﻿with CTE AS
+(select
+1 n, cast (0 as varchar(16)) bit_or,
+code, speed, ram FROM PC
+UNION ALL
+select n*2,
+cast (convert(bit,(speed|ram)&n) as varchar(1))+cast(bit_or as varchar(15))
+, code, speed, ram
+from CTE where n < 65536
+)
+select code, speed, ram from CTE
+where n = 65536
+and CHARINDEX('1111', bit_or )> 0
+
+-- : TODO Something with CHARINDEX
