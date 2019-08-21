a2a5163f1f284b7030d25325df90350a1743827d
shinbatsu <nojic132@gmail.com>
2019-08-21 20:48:48 +0300

    solve 136

---
+++ 136.sql
@@ -0,0 +1,6 @@
+SELECT s.name, d.pat, SUBSTRING(s.name,d.pat, 1) t  FROM Ships s
+CROSS APPLY ( 
+	VALUES(PATINDEX('%[" "-,.*!@#$^&()''""|\/_`~<>;:}{+=1234567890]%', s.name)) 
+		)
+	AS d(pat)
+WHERE d.pat <> 0
