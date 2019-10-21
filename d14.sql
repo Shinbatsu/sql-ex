b01ae8461a16ddd75e98650c1e39be2ae52d4075
shinbatsu <nojic132@gmail.com>
2019-10-21 19:30:22 +0300

    solve DML 14

---
+++ d14.sql
@@ -0,0 +1,25 @@
+DELETE FROM Classes
+WHERE
+  CLASS in (
+    SELECT
+      c.class
+    FROM
+      (
+        SELECT
+          name,
+          CLASS
+        FROM
+          Ships
+        UNION
+        SELECT
+          ship,
+          ship
+        FROM
+          Outcomes
+      ) AS sh
+      RIGHT JOIN Classes c ON c.class = sh.class
+    GROUP BY
+      c.class
+    HAVING
+      count(sh.name) <= 2
+  )
