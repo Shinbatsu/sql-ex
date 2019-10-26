724afef68fd949b0a34450825e36bc061d7aaa1e
shinbatsu <nojic132@gmail.com>
2019-10-26 17:30:55 +0300

    solve DML 16

---
+++ d16.sql
@@ -0,0 +1,18 @@
+DELETE FROM Product
+WHERE
+  model not in (
+    SELECT
+      model
+    FROM
+      PC
+    UNION
+    SELECT
+      model
+    FROM
+      Laptop
+    UNION
+    SELECT
+      model
+    FROM
+      Printer
+  )
